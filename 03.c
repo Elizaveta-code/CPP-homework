31a6b9c2aaf469d5810e53b47b49e8ffe373a6b0
Elizaveta-code <elizavetaragozina69@gmail.com>
2025-09-22 10:48:11 +0300

    Add files via upload

---
+++ 03.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+float yearfrac(int year, int day)
+{
+    int days_in_year;
+    if (year % 4 == 0) {
+        days_in_year = 366;
+    } else {
+        days_in_year = 365;
+    }
+    return (float)day / days_in_year;
+}
+
+int main()
+{
+    int year, day;
+    scanf("%i", &year);
+    scanf("%i", &day);
+    printf("%.5f", yearfrac(year, day));
+}
