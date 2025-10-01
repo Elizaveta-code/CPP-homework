1298d08fec8ddf2b3db8b6fbeafad381fc0dc9cf
Elizaveta-code <elizavetaragozina69@gmail.com>
2025-10-01 22:15:41 +0300

    11.c

--- 11.c
+++ 11.c
@@ -1,23 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 
-void safe_strcpy(char* new, size_t new_size, const char* old)
+void safe_strcpy(char* destination, size_t new_size, const char* source)
 {
     if (new_size == 0)
         return;
     size_t i;
-    for (i = 0; (i < new_size - 1) && (old[i] != '\0'); i++)
-        new[i] = old[i];
-    new[i] = '\0';
+    for (i = 0; (i < new_size - 1) && (source[i] != '\0'); i++)
+        destination[i] = source[i];
+    destination[i] = '\0';
+    printf("%s", destination);
 }
 
 int main()
 {
-    char a[8] = "mouse";
-    char b[50] = "LARGEmouse";
-    
-    printf("Before:'%s'\n", a);
+    char a[10] = "Mouse";
+    char b[50] = "LargeElephant";
     safe_strcpy(a, sizeof(a), b);
-    printf("After:'%s'\n", a);
-    printf("Length: %zu\n", strlen(a));
 }
