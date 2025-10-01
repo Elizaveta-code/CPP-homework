13fb39156fe5a6fcdb37a2344794815c9f038130
Elizaveta-code <elizavetaragozina69@gmail.com>
2025-10-01 23:53:20 +0300

    13.c

--- 13.c
+++ 13.c
@@ -3,76 +3,70 @@
 #include <string.h>
 #include <ctype.h>
 
-int is_integer(const char *str)
-{
-    if (str == NULL || *str == '\0')
+int is_integer(const char *str) {
+    if (str == NULL || *str == '\0') {
         return 0;
-    if (*str == '+' || *str == '-')
-        str++;
-    while (*str)
-    {
-        if (!isdigit(*str))
+    }
+    int i = 0;
+    if (str[i] == '+' || str[i] == '-') {
+        i++;
+    }
+    if (str[i] == '\0') {
+        return 0;
+    }
+    for (; str[i] != '\0'; i++) {
+        if (!isdigit(str[i])) {
             return 0;
-        str++;
+        }
     }
     return 1;
 }
 
-int main(int argc, char** argv)
-{
-    if (argc != 4)
-    {
+int main(int argc, char *argv[]) {
+    if (argc != 4) {
         printf("Error: Wrong number of arguments!\n");
         printf("Usage: %s <number> <operator> <number>\n", argv[0]);
-        return 0;
+        return 1;
     }
-    if (!is_integer(argv[1]) || !is_integer(argv[3]))
-    {
-        printf("Error: Invalid number format!\n");
-        return 0;
+    
+    if (!is_integer(argv[1]) || !is_integer(argv[3])) {
+        printf("Error: Operands should be integers!");
+        return 1;
+    }
+    if (strlen(argv[2]) != 1) {
+        printf("Error: Operator must be a single character!\n");
+        return 1;
     }
     int num1 = atoi(argv[1]);
     int num2 = atoi(argv[3]);
-
-    if (strlen(argv[2]) != 1)
-    {
-        printf("Error: Invalid operator, not a single char!\n");
-        return 0;
-    }
-    
     char operator = argv[2][0];
-    int result = 0;
-    switch (operator)
-    {
+    
+    switch (operator) {
         case '+':
-            result = num1 + num2;
+            printf("%d\n", num1 + num2);
             break;
         case '-':
-            result = num1 - num2;
+            printf("%d\n", num1 - num2);
             break;
         case '*':
-            result = num1 * num2;
+            printf("%d\n", num1 * num2);
             break;
         case '/':
-            if (num2 == 0)
-            {
+            if (num2 == 0) {
                 printf("Error: Division by zero!\n");
-                return 0;
+                return 1;
             }
-            result = num1 / num2;
+            printf("%d\n", num1 / num2);
             break;
         case '%':
-            if (num2 == 0)
-            {
-                printf("Error: Modul by zero!\n");
-                return 0;
+            if (num2 == 0) {
+                printf("Error: Division by zero!\n");
+                return 1;
             }
-            result = num1 % num2;
+            printf("%d\n", num1 % num2);
             break;
         default:
-            printf("Error: Invalid operator! Use +, -, *, /, or %%\n");
+            printf("Error: Invalid operator '%c'!\n", operator);
             return 1;
     }
-    
-    printf("%i\n", result);
 }
